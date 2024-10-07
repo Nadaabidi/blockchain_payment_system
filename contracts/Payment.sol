b3e488c6fb103d01e03de00661198c6fec97f19f
Nadaabidi <abidinada51@gmail.com>
2024-10-07 05:35:36 +0200

    Add other functions to the contract

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -4,6 +4,18 @@ pragma solidity ^0.8.0;
 contract Payment {
     address public owner;
 
+    uint256 public paymentLimit;
+
+    mapping(address => uint256) public balances;
+
+    struct PaymentInfo {
+        address sender;
+        uint256 amount;
+        uint256 timestamp;
+    }
+
+    PaymentInfo[] public payments;
+
     event PaymentReceived(address indexed sender, uint256 amount);
 
     constructor() {
@@ -24,5 +36,34 @@ contract Payment {
     // Get the balance of the contract
     function getBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function setPaymentLimit(uint256 _limit) public {
+        require(msg.sender == owner, "Only the owner can set the payment limit");
+        paymentLimit = _limit;
+    }
+
+    function sendPayment() public payable {
+        require(msg.value <= paymentLimit, "Payment exceeds the limit");
+        balances[msg.sender] += msg.value;
+        payments.push(PaymentInfo(msg.sender, msg.value, block.timestamp)); 
+        emit PaymentReceived(msg.sender, msg.value);
+    }
+
+    function refund() public {
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, "No balance to refund");
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function getPayment(uint256 index) public view returns (address, uint256, uint256) {
+        PaymentInfo memory payment = payments[index]; 
+        return (payment.sender, payment.amount, payment.timestamp);
+    }
+
+    
+    function getPaymentCount() public view returns (uint256) {
+        return payments.length;
     }
 }
